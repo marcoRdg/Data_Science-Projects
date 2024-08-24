d2261c22ccf9fbcfc5446582954802801a4723ce
marcoRdg <marco_ant.lopes@yahoo.com>
2024-08-24 01:52:42 -0300

    adicionando novo projeto

---
+++ Web_Scraping_Steam-Twitch_Streamlit/main.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+
+
+def run_streamlit_app(script_name):
+    try:
+        # Comando para rodar o streamlit
+        command = f"streamlit run {script_name}"
+
+        # Abre o streamlit no navegador padrão
+        subprocess.run(command, shell=True)
+
+    except Exception as e:
+        print(f"Erro ao tentar abrir o Streamlit: {e}")
+
+
+if __name__ == "__main__":
+    # Caminho do arquivo do dashboard dentro da pasta 'library'
+    script_name = "library/dashboard.py"
+
+    # Chama a função para abrir o Streamlit
+    run_streamlit_app(script_name)
